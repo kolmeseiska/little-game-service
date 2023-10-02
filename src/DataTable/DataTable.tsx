b5d30f4095f3d84e1f2c64167ec8619f41444cca
niemisami <sami@nieminen.fi>
2023-10-02 21:30:54 +0300

    Create simple DataTable component

---
+++ src/DataTable/DataTable.tsx
@@ -0,0 +1,28 @@
+import { Table, TableContainer, Tbody, Thead } from '@chakra-ui/react'
+
+type Props = {
+  id?: string,
+  headers: React.JSX.Element | React.JSX.Element[],
+  rows: React.JSX.Element | React.JSX.Element[],
+  extras: React.JSX.Element | React.JSX.Element[],
+  className?: string,
+  size?: string[],
+  variant?: 'simple' | 'striped' | 'unstyled'
+}
+
+const DataTable = ({ id, headers, rows, extras, className, size = ['sm', 'md'], variant = 'simple' }: Props) => {
+  return (
+    <TableContainer id={id} overflowX='auto' className={className}>
+      <Table variant={variant} size={size}>
+        <Thead>
+          {headers}
+        </Thead>
+        <Tbody>
+          {rows}
+        </Tbody>
+      </Table>
+      {extras}
+    </TableContainer>
+  )
+}
+export default DataTable
